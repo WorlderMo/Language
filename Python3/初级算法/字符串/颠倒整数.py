ecb5498e1bd2e4295c66f9673ad4e6d5ee52ffd8
mohailang <1198534595@qq.com>
2018-05-03 15:03:03 +0800

    5.3

--- Python3/初级算法/字符串/颠倒整数.py
+++ Python3/初级算法/字符串/颠倒整数.py
@@ -9,14 +9,39 @@ class Solution:
         :rtype: int
         """
         # 字符串法
-        result = ""
-        xlist = list(str(abs(x)))
-        xlist.reverse()
-        for i in xlist:
-            result += i
-        result = int(result)
+        # result = ""
+        # xlist = list(str(abs(x)))
+        # xlist.reverse()
+        # for i in xlist:
+        #     result += i
+        # result = int(result)
 
-        result = result if x > 0 else -result
-        if result < -(1 << 31) or result > (1 << 31):
+        # result = result if x > 0 else -result
+        # if result < -(1 << 31) or result > (1 << 31):
+        #     return 0
+        # return result
+
+        # 直接倒序
+        # x_str = str(x)
+        # is_pos = x >= 0
+        # x_str = x_str if is_pos else x_str[1:]
+        # x_str = (int)(x_str[::-1])
+        # if x_str > (1 << 31) - 1:
+        #     return 0
+
+        # return x_str if is_pos else -x_str
+
+        # 取余法  就是用数学方法获取各位上的数字
+        if x == 0:
             return 0
-        return result
+        num = 0
+        neg = 1
+        if x < 0:
+            neg, x = -1, -x
+        while x > 0:
+            num = num*10 + x % 10
+            x //= 10
+        num *= neg
+        if num < -(1 << 31) or num > (1 << 31)-1:
+            return 0
+        return num
