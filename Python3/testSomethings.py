0f139ae09da2eca8f906564cb81b657ec1160dd3
mohailang <1198534595@qq.com>
2018-05-07 20:08:27 +0800

    5.7

--- Python3/testSomethings.py
+++ Python3/testSomethings.py
@@ -1,4 +1,5 @@
-a = "hello"
-b = "lle"
-c = a.find(b)
-print(c)
+height = [[]]
+n = int(raw_input("n:"))
+for i in range(n):
+    for j in range(n):
+        height[i].append(int(raw_input()))
--- Python3/初级算法/字符串/数数并说.py
+++ Python3/初级算法/字符串/数数并说.py
@@ -1,3 +1,30 @@
 # -*- coding: utf-8 -*-
 # @Date    : 2018-05-04 23:03:42
 # @Author  : mohailang (1198534595@qq.com)
+
+
+class Solution:
+    def count(self, s):
+        strNum = ''
+        count = 0
+        temp = s[0]
+        size = len(s)
+        for i in range(0, size):
+            if temp == s[i]:
+                count += 1
+            else:
+                strNum += str(count)+temp
+                temp = s[i]
+                count = 1
+        strNum += str(count)+temp
+        return strNum
+
+    def countAndSay(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        s = "1"
+        for i in range(2, n+1):
+            s = self.count(s)
+        return s
