9ac062261ee877c75d38030b5bc4daef0a6425d7
mohailang <1198534595@qq.com>
2018-05-20 01:32:27 +0800

    5.20

--- Python3/testSomethings.py
+++ Python3/testSomethings.py
@@ -1,1 +1,26 @@
-print("aaaa")
+import glob
+import os
+import django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "WQSiteNavigator.settings")
+
+
+django.setup()
+
+
+def readDocument(filename):
+    from main.models import Document
+    with open(filename, 'rb') as f:
+        content = f.read()
+    title = os.path.splitext(filename.split('/')[-1])[0]
+    Document.objects.create(tilte=title, content=content)
+
+
+def function(path):
+    for filename in glob.glob(path + os.sep + '*'):
+        if os.path.isdir(filename):
+            function(filename)
+        else:
+            readDocument(filename)
+
+
+function(r'/Users/worlder/Documents/Language/Python2/WQSiteNavigator/main/static/files/')
