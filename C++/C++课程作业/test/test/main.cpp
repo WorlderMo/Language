9d820b60aafaccbf358332dc36599d0f9db04cf7
mohailang <1198534595@qq.com>
2019-01-13 23:11:39 +0800

    1.13

--- C++/C++课程作业/test/test/main.cpp
+++ C++/C++课程作业/test/test/main.cpp
@@ -7,9 +7,47 @@
 //
 
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+class Person
+{
+public:
+    Person()
+    {
+        name = new char[16];
+        cout << "Person构造" << endl;
+    }
+    virtual ~Person()
+    {
+        delete[] name;
+        cout << "Person析构" << endl;
+    }
+    
+private:
+    char *name;
+};
+class Teacher : virtual public Person
+{
+public:
+    Teacher() { cout << "Teacher构造" << endl; }
+    ~Teacher() { cout << "Teacher析构" << endl; }
+};
+class Student : virtual public Person
+{
+public:
+    Student() { cout << "Student构造" << endl; }
+    ~Student() { cout << "Student析构" << endl; }
+};
+class TS : public Teacher, public Student
+{
+public:
+    TS() { cout << "TS构造" << endl; }
+    ~TS() { cout << "TS析构" << endl; }
+};
+int main(int argc, char *argv[])
+{
+    Person *p = new TS();
+    delete p;
     return 0;
 }
+
+
